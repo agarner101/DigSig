fea82f1413f8eb4d03669655abded3f98b79607c
Andrew Garner <andrewgarner101@gmail.com>
2018-08-08 12:21:02 -0700

    Initial project check in

---
+++ app/src/main/java/io/andrewgarner/digsig/MainActivity.kt
@@ -0,0 +1,17 @@
+package io.andrewgarner.digsig
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Main Activity
+ *
+ * Created by Andrew Garner on 8/8/2018
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
